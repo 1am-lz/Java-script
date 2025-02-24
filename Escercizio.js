fb02fed70af814094629a5d5c06d78696cf725ed
lorenz <renzosena1@gmail.com>
2025-02-24 18:02:59 +0100

    esercizio js

---
+++ Escercizio.js
@@ -0,0 +1,9 @@
+let nomestudente = "lorenz";
+let etastudente = 20;
+let corsoattivo = true; // booleano
+
+console.log("il nome dello studente è:",nomestudente) ;
+console.log("L'età dello studente è:",etastudente);
+console.log("lo studente e iscritto al corso?")
+
+
